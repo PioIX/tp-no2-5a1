e25059542f29a9feb0c9bf948f120aa82dc1de49
altman44 <48921520+altman44@users.noreply.github.com>
2019-05-16 22:16:32 -0300

    16-5

--- Bingo-2019/Bingo-2019/Cartel.Designer.vb
+++ Bingo-2019/Bingo-2019/Cartel.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Cartel
     Inherits System.Windows.Forms.Form
 
     'Form reemplaza a Dispose para limpiar la lista de componentes.
@@ -22,9 +22,29 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TxtNumber = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'TxtNumber
+        '
+        Me.TxtNumber.Location = New System.Drawing.Point(57, 86)
+        Me.TxtNumber.Multiline = True
+        Me.TxtNumber.Name = "TxtNumber"
+        Me.TxtNumber.Size = New System.Drawing.Size(70, 70)
+        Me.TxtNumber.TabIndex = 0
+        '
+        'Cartel
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1069, 716)
+        Me.Controls.Add(Me.TxtNumber)
+        Me.Name = "Cartel"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TxtNumber As TextBox
 End Class
